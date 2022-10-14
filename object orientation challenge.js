f5cb37226e2e803b5a02c9aba2cf1a5c78150749
alicia fernandez <fernandez.m.alicia2020@gmail.com>
2022-10-14 02:22:39 -0400

    first commit adding object orientation challenge assignment umass global SE program

---
+++ object orientation challenge.js
@@ -0,0 +1,58 @@
+// JavaScript source code
+
+// object orientation challenge
+
+
+const raceResults = ([first, second, third, ...rest]) => ({ first, second, third, rest });
+
+class Vehicle {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    toString() {
+        return `The vehicle is a ${this.make} ${this.model} from ${this.year}!`;
+    }
+    honk() {
+        return "Beep!";
+    }
+}
+class Car extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 4;
+    }
+    
+}
+
+class Motorcycle extends Vehicle {
+    constructor(make, model, year) {
+        super(make, model, year);
+        this.numWheels = 2;
+        
+    }
+    revEngine() {
+        return "VROOM!!!";
+    }
+}
+
+class Garage {
+    constructor(capacity) {
+        this.vehicles = [];
+        this.capacity = capacity;
+    }
+    add(newVehicle) {
+        if (newVehicle instanceof Vehicle == false) {
+            return "Vehicles only!";
+        }
+        if (this.vehicles.length >= this.capacity) {
+            return "no more room!";
+        }
+        
+        this.vehicles.push(newVehicle);
+        return "Vehicle parked!"
+    }
+    
+
+}
